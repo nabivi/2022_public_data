649005e2d6227dfdb3a092784a97ef2e04c48998
nabivi <nerdring54@gmail.com>
2022-09-24 04:13:21 +0900

    페이지 라우트 추가

---
+++ client/src/pages/Classifier.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ClassifierPage () {
+  return (
+    <div>분류기 페이지</div>
+  )
+}
+
+export default ClassifierPage;
---
+++ client/src/pages/Menu.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MenuPage () {
+  return (
+    <div>메인 페이지</div>
+  )
+}
+
+export default MenuPage;
