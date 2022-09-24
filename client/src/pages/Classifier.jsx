51d670574b9c1131548853aa82fad0f1717d6c9a
nabivi <nerdring54@gmail.com>
2022-09-24 19:56:29 +0900

    분류기 페이지 추가

--- client/src/pages/Classifier.jsx
+++ client/src/pages/Classifier.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from '../components/NavBar';
+import Footer from '../components/Footer';
+import ClassifierImg from '../assets/classifier.png'
 
 function ClassifierPage () {
+  const [fileImage, setFileImage] = useState('');
+
   return (
-    <div>분류기 페이지</div>
+    <div className='w-full h-fit min-h-screen flex flex-col gap-8 justify-between items-center'>
+      <Navbar />
+      <div className='w-full h-fit max-w-md flex flex-col items-center gap-5'>
+        <img src={ClassifierImg} className='w-[250px]' />
+        <form className='w-full px-5 flex flex-col gap-5 items-center'>
+          <label for='file'>
+            <div className='btn text-white'>사진 업로드!</div>
+          </label>
+          <input
+            type='file'
+            name='file'
+            id='file'
+            accept='image/*'
+            className='hidden'
+            onChange={(e) => setFileImage(URL.createObjectURL(e.target.files[0]))} />
+          <img src={fileImage} className='w-full'/>
+        </form>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
