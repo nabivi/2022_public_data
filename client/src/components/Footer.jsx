ce83aa83d00c03a670704a7f5a7dea5f36adefc8
nabivi <nerdring54@gmail.com>
2022-09-25 13:43:59 +0900

    footer 누르면 홈으로 이동

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import FooterLogo from "../assets/footer.png"
 
 function Footer () {
   return (
     <footer className="w-full h-16 max-w-md flex justify-center">
-      <img src={FooterLogo} className="h-full" />
+      <Link to='/'>
+        <img src={FooterLogo} className="h-full" />
+      </Link>
     </footer>
   )
 }
